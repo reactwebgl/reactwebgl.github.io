6772bec78d19fa6b6bac790ddca14862fd8870d1
stockdepot <itcwangfeng@hotmail.com>
2021-06-15 13:04:33 -0500

    webgl in react

---
+++ precache-manifest.7ba21592f1ac662db3d32a07b607c185.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d07cda639eb3658b9c7d19fcfff1d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2469098908beafbe30",
+    "url": "/static/css/main.bb6da3ec.chunk.css"
+  },
+  {
+    "revision": "696b681a66188041f999",
+    "url": "/static/js/2.29c85c6c.chunk.js"
+  },
+  {
+    "revision": "fc521b05ccaced4754c9cbde9a95c1d6",
+    "url": "/static/js/2.29c85c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2469098908beafbe30",
+    "url": "/static/js/main.db738d9d.chunk.js"
+  },
+  {
+    "revision": "a32e939adfb2c82f8235",
+    "url": "/static/js/runtime-main.18294a5f.js"
+  },
+  {
+    "revision": "5e2a26d1a65ed5f51c54d1da661bb067",
+    "url": "/static/media/Sphere512.5e2a26d1.png"
+  },
+  {
+    "revision": "f1a8f733d7ba3a83c3f40ca67857ad22",
+    "url": "/static/media/checkWhite.f1a8f733.svg"
+  },
+  {
+    "revision": "33500f11a53792d8f6ab988f9ce43a03",
+    "url": "/static/media/checked.33500f11.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/static/media/down.ca82bc74.svg"
+  },
+  {
+    "revision": "51f82a79b7b7ac5e9d755162479ddf77",
+    "url": "/static/media/event.51f82a79.svg"
+  },
+  {
+    "revision": "27fa1f2d2c955d7c173539daa79883c6",
+    "url": "/static/media/fracture.27fa1f2d.svg"
+  },
+  {
+    "revision": "246118d2f96922ccc027e20a4a4449eb",
+    "url": "/static/media/geophone.246118d2.svg"
+  },
+  {
+    "revision": "be35c354f8da9e6fcd8eeef62ad323ec",
+    "url": "/static/media/home01.be35c354.png"
+  },
+  {
+    "revision": "195b60f7d434747b62f7db52ba8c513e",
+    "url": "/static/media/home02.195b60f7.png"
+  },
+  {
+    "revision": "1a08c8ddf8c794f75d00b3ebdab1c509",
+    "url": "/static/media/home03.1a08c8dd.png"
+  },
+  {
+    "revision": "a3ceb2f8ec3556625fb06e1b1c73a88f",
+    "url": "/static/media/home04.a3ceb2f8.png"
+  },
+  {
+    "revision": "663cfbdb7170a96fa6a6d2e53ed77d65",
+    "url": "/static/media/las.663cfbdb.svg"
+  },
+  {
+    "revision": "364446a7ed652226de73d4eece383ab4",
+    "url": "/static/media/perf.364446a7.svg"
+  },
+  {
+    "revision": "8194860f86fdaee8de48d3e3343308f7",
+    "url": "/static/media/project.8194860f.svg"
+  },
+  {
+    "revision": "c55712b8d1d505cc02c0ad9efb9db8f9",
+    "url": "/static/media/pushpin.c55712b8.svg"
+  },
+  {
+    "revision": "852730108c9eda394f0ef5b3214e106c",
+    "url": "/static/media/pushpin2.85273010.svg"
+  },
+  {
+    "revision": "cb750aeff95ed6e13a93d8afd5350489",
+    "url": "/static/media/right.cb750aef.svg"
+  },
+  {
+    "revision": "23950b863fe472206377261c6db59b2a",
+    "url": "/static/media/wellbore.23950b86.svg"
+  },
+  {
+    "revision": "709ca952a6424823c0754a79f1bd1d08",
+    "url": "/static/media/wellflag.709ca952.svg"
+  }
+]);
